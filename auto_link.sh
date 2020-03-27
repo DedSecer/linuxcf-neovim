ea6dcc846fa38cbb0e06b2bac0da6c85c4fe2c86
DedSecer <z2465216809qq.com@icloud.com>
2020-03-27 18:26:46 +0800

    update the way of ln

--- auto_link.sh
+++ auto_link.sh
@@ -2,16 +2,16 @@
 nvim_path=~/.config/nvim
 
 mkdir -p ${nvim_path}
-ln ./init.vim ./coc-settings.json ${nvim_path}/
+ln -i ./init.vim ./coc-settings.json ${nvim_path}/
 
 mkdir -p ${nvim_pat}h/autoload
-ln ./autoload/plug.vim ${nvim_pat}h/autoload
+ln -i ./autoload/plug.vim ${nvim_pat}h/autoload
 
 ps_path=plugged_scripts
 mkdir -p ${nvim_path}/${ps_path}
 for file in `ls ./${ps_path}`  
 do
-	ln "./${ps_path}/${file}"  "${nvim_path}/${ps_path}/"
+	ln -i "./${ps_path}/${file}"  "${nvim_path}/${ps_path}/"
 done	
 
 

cf243ab0b0b2ad2eabfbd2b3da93ea45cd0b1b6b
DedSecer <z2465216809qq.com@icloud.com>
2020-04-15 00:13:34 +0800

    update ud_ln.sh

--- ud_ln.sh
+++ ud_ln.sh
@@ -2,23 +2,24 @@
 
 #update file from github and make hard link to your config directory
 
-nvim_path=~/.config/nvim
-ln_cmd='ln -i'
-ps_path=plugged_scripts
+nvim_path="${HOME}/.config/nvim"
+ln_cmd='ln -f'
 
-git pull
+function lnr() {
+	#a function to make hardlinked_subtree
+	#${1}:from path , ${2}:to path , ${3}:ln_cmd
 
-mkdir -p ${nvim_path}
+	if test -d ${1}; then
+		for file in `ls ${1}` ; do
+			mkdir -p ${2}
+			lnr ${1}/${file} ${2}/${file} "${3}"
+			done	
+	
+	elif test -f ${1}; then
+		 ${3} ${1} ${2}
+	fi		
+}
 
-${ln_cmd} ./init.vim ./coc-settings.json ${nvim_path}/
+#git pull
 
-mkdir -p ${nvim_path}/autoload
-${ln_cmd} ./autoload/plug.vim ${nvim_path}/autoload/
-
-mkdir -p ${nvim_path}/${ps_path}
-for file in `ls ./${ps_path}`  
-do
-	${ln_cmd} "./${ps_path}/${file}"  "${nvim_path}/${ps_path}/"
-done	
-
-
+lnr . ${nvim_path} "${ln_cmd}"
